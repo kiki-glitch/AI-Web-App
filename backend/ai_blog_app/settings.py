7358c431fc1c49dca77819e3871fda007147a149
unknown <kikialex37@gmail.com>
2025-07-14 17:14:02 +0300

    DB Connection

--- backend/ai_blog_app/settings.py
+++ backend/ai_blog_app/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Covid192025#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
