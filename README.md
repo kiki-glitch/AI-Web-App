dc8741324e9fded0cba2fa3c9aa33495efc62730
unknown <kikialex37@gmail.com>
2025-07-16 13:11:56 +0300

    Updated Readme file

--- README.md
+++ README.md
@@ -110,3 +110,9 @@ This project is for learning and portfolio purposes. You are free to fork and ex
 - [ ] Add blog post editing or export
 - [ ] Add dark mode theme
 - [ ] Dockerize for easier deployment
+- [ ] Use Celery + Redis to handle YouTube download, transcription, and blog generation in the background. This avoids long wait times during form submission.
+- [ ] Limit YouTube video length (e.g. max 10 minutes) to speed up API calls.
+
+- [ ] Cache processed YouTube links by ID to reuse previous outputs.
+
+- [ ] Add performance profiling to each step to track slow operations.
--- backend/ai_blog_app/settings.py
+++ backend/ai_blog_app/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zfq=fv%x65h90k8c$#583ssev9+xg&3tz7l-rz%r@7w#aj3yx&'
 
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+# DEBUG = os.getenv("DEBUG") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,14 +86,25 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ai_blog_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Covid192025#',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ai_blog_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Covid192025#',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
